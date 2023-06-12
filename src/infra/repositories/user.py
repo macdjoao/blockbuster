860a8558b3a951f6ec70fdfe58cf9632252660b9
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-12 18:56:11 -0300

    feat: select e insert de User Repository

---
+++ src/infra/repositories/user.py
@@ -0,0 +1,50 @@
+from src.infra.configs.session import session
+from src.infra.entities.models import User as UserEntity
+
+
+class User:
+    def select(
+        self,
+        id: str = None,
+        email: str = None,
+        name: str = None,
+        is_active: bool = None,
+    ):
+        try:
+            custom_filter = session.query(UserEntity)
+            if id is not None:
+                custom_filter = custom_filter.filter(UserEntity.id == id)
+            if email is not None:
+                custom_filter = custom_filter.filter(UserEntity.email == email)
+            if name is not None:
+                custom_filter = custom_filter.filter(UserEntity.name == name)
+            if is_active is not None:
+                custom_filter = custom_filter.filter(
+                    UserEntity.is_active == is_active
+                )
+
+            data_select = custom_filter.all()
+
+            return data_select
+
+        except Exception as err:
+            return err
+        finally:
+            session.close()
+
+    def insert(self, email: str, name: str, password: str):
+        try:
+            data_insert = UserEntity(
+                email=email.lower(),
+                name=name.capitalize(),
+                password=password,
+            )
+            session.add(data_insert)
+            session.commit()
+
+            return data_insert
+
+        except Exception as err:
+            return err
+        finally:
+            session.close()
