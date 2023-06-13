9d8f6e581b4ab3e9fac225ce024f6eab0b995430
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-13 16:59:23 -0300

    feat: Adicionando repr de Rent entity

--- src/infra/entities/rent.py
+++ src/infra/entities/rent.py
@@ -13,3 +13,6 @@ class Rent(Base):
     rent_date = Column(DateTime(timezone=True), server_default=func.now())
     devolution_date = Column(DateTime(timezone=True), default=None)
     finished = Column(Boolean, default=True)
+
+    def __repr__(self):
+        return f'Rent (id = {self.id}, user = {self.user}, customer = {self.customer}, movie = {self.movie}, rent_date = {self.rent_date}, devolution_date = {self.devolution_date}, finished = {self.finished})'
