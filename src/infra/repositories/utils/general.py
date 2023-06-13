b5be68e9ff821c746921b87c54025a9906fe3640
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-13 20:04:51 -0300

    feat: util params_is_none, param_is_not_a_string, param_is_not_a_date

---
+++ src/infra/repositories/utils/general.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+
+def params_is_none(*args):
+    for arg in args:
+        if arg == None:
+            return True
+    return False
+
+
+def param_is_not_a_string(*args):
+    for arg in args:
+        if not isinstance(arg, str):
+            return True
+    return False
+
+
+def param_is_not_a_date(*args):
+    for arg in args:
+        if not isinstance(arg, date):
+            return True
+    return False
