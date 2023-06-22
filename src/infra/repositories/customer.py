94d65ef65ab26c77005436ec72467211668c144e
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-22 20:53:47 -0300

    fix: Refatorando CustomerRepository

--- src/infra/repositories/customer.py
+++ src/infra/repositories/customer.py
@@ -6,11 +6,6 @@ from src.infra.repositories.errors.general import (EmailAlreadyRegisteredError,
                                                    ParamIsNotBoolError,
                                                    ParamIsNotIntegerError,
                                                    ParamIsNotStringError)
-from src.infra.repositories.utils.general import (email_already_registered,
-                                                  id_not_found, param_is_none,
-                                                  param_is_not_a_bool,
-                                                  param_is_not_a_int,
-                                                  param_is_not_a_string)
 
 customer_entity = CustomerEntity()
 
@@ -25,6 +20,14 @@ class CustomerRepository:
             )
             if data_email is not None:
                 raise EmailAlreadyRegisteredError(email=email)
+
+            if type(email) is not str:
+                raise ParamIsNotStringError(arg=email)
+            if type(first_name) is not str:
+                raise ParamIsNotStringError(arg=first_name)
+            if type(last_name) is not str:
+                raise ParamIsNotStringError(arg=last_name)
+
             data_insert = CustomerEntity(
                 email=email.lower(),
                 first_name=first_name.capitalize(),
@@ -32,10 +35,12 @@ class CustomerRepository:
             )
             session.add(data_insert)
             session.commit()
-
             return data_insert
 
         except EmailAlreadyRegisteredError as err:
+            session.rollback()
+            return err.message
+        except ParamIsNotStringError as err:
             session.rollback()
             return err.message
         finally:
@@ -51,37 +56,41 @@ class CustomerRepository:
     ):
         try:
             custom_filter = session.query(CustomerEntity)
+
             if id is not None:
-                if param_is_not_a_int(id):
+                if type(id) is not int:
                     raise ParamIsNotIntegerError(arg=id)
                 custom_filter = custom_filter.filter(CustomerEntity.id == id)
+
             if email is not None:
-                if param_is_not_a_string(email):
+                if type(email) is not str:
                     raise ParamIsNotStringError(arg=email)
                 custom_filter = custom_filter.filter(
                     CustomerEntity.email == email
                 )
+
             if first_name is not None:
-                if param_is_not_a_string(first_name):
+                if type(first_name) is not str:
                     raise ParamIsNotStringError(arg=first_name)
                 custom_filter = custom_filter.filter(
                     CustomerEntity.first_name == first_name
                 )
+
             if last_name is not None:
-                if param_is_not_a_string(last_name):
+                if type(last_name) is not str:
                     raise ParamIsNotStringError(arg=last_name)
                 custom_filter = custom_filter.filter(
                     CustomerEntity.last_name == last_name
                 )
+
             if is_active is not None:
-                if param_is_not_a_bool(is_active):
+                if type(is_active) is not bool:
                     raise ParamIsNotBoolError(arg=is_active)
                 custom_filter = custom_filter.filter(
                     CustomerEntity.is_active == is_active
                 )
 
             data_select = custom_filter.all()
-
             return data_select
 
         except ParamIsNotIntegerError as err:
@@ -98,7 +107,7 @@ class CustomerRepository:
 
     def update(
         self,
-        id: int = None,
+        id: int,
         email: str = None,
         first_name: str = None,
         last_name: str = None,
@@ -106,9 +115,7 @@ class CustomerRepository:
     ):
 
         try:
-            if param_is_none(id):
-                raise IncompleteParamError(arg=id)
-            if param_is_not_a_int(id):
+            if type(id) is not int:
                 raise ParamIsNotIntegerError(arg=id)
             data_id = (
                 session.query(CustomerEntity)
@@ -117,34 +124,43 @@ class CustomerRepository:
             )
             if data_id is None:
                 raise IdNotFoundError(id=id)
+
             if email is not None:
-                if param_is_not_a_string(email):
+                if type(email) is not str:
                     raise ParamIsNotStringError(arg=email)
-                if email_already_registered(
-                    session=session, object=customer_entity, email=email
-                ):
+                data_email = (
+                    session.query(CustomerEntity)
+                    .filter(CustomerEntity.email == email)
+                    .first()
+                )
+                if data_email is not None:
                     raise EmailAlreadyRegisteredError(email=email)
                 session.query(CustomerEntity).filter(
                     CustomerEntity.id == id
                 ).update({'email': email})
+
             if first_name is not None:
-                if param_is_not_a_string(first_name):
+                if type(first_name) is not str:
                     raise ParamIsNotStringError(arg=first_name)
                 session.query(CustomerEntity).filter(
                     CustomerEntity.id == id
                 ).update({'first_name': first_name})
+
             if last_name is not None:
-                if param_is_not_a_string(last_name):
+                if type(last_name) is not str:
                     raise ParamIsNotStringError(arg=last_name)
                 session.query(CustomerEntity).filter(
                     CustomerEntity.id == id
                 ).update({'last_name': last_name})
+
             if is_active is not None:
-                if param_is_not_a_bool(is_active):
+                if type(is_active) is not bool:
                     raise ParamIsNotBoolError(arg=is_active)
                 session.query(CustomerEntity).filter(
                     CustomerEntity.id == id
                 ).update({'is_active': is_active})
+
+            session.commit()
 
             data_update = (
                 session.query(CustomerEntity)
@@ -176,16 +192,16 @@ class CustomerRepository:
 
     def delete(self, id: int):
         try:
-            if param_is_none(id):
+            if id is None:
                 raise IncompleteParamError(arg=id)
-            if param_is_not_a_int(id):
+            if type(id) is not int:
                 raise ParamIsNotIntegerError(arg=id)
             data_id = (
                 session.query(CustomerEntity)
                 .filter(CustomerEntity.id == id)
                 .first()
             )
-            if data_id is not None:
+            if data_id is None:
                 raise IdNotFoundError(id=id)
 
             session.query(CustomerEntity).filter(
