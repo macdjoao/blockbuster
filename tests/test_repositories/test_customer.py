f04c8b3b811bb195552c8c5eb5f90f5f328ed86a
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-22 20:54:17 -0300

    tests: test_update

--- tests/test_repositories/test_customer.py
+++ tests/test_repositories/test_customer.py
@@ -7,48 +7,68 @@ fake = Faker()
 
 
 def test_insert():
+    # Fake payload
     fake_email = fake.email()
     fake_first_name = fake.first_name()
     fake_last_name = fake.last_name()
 
+    # Inserting fake registry
     customer_repository.insert(
         email=fake_email, first_name=fake_first_name, last_name=fake_last_name
     )
 
+    # Selecting fake registry
     query = customer_repository.select(email=fake_email)
 
+    # Checking equalities
     assert query[0].email == fake_email
     assert query[0].first_name == fake_first_name
     assert query[0].last_name == fake_last_name
 
+    # Cleaning DB
+    customer_repository.delete(id=(query[0].id))
+
 
 def test_insert_EmailAlreadyRegisteredError():
+    # Fake payload
     fake_email = fake.email()
     fake_first_name = fake.first_name()
     fake_last_name = fake.last_name()
+    # Inserting fake registry
     customer_repository.insert(
         email=fake_email, first_name=fake_first_name, last_name=fake_last_name
     )
 
+    # Second fake Payload
     second_fake_first_name = fake.first_name()
     second_fake_last_name = fake.last_name()
+    # Trying insert second fake registry with same email of first fake registry
     fail_insert = customer_repository.insert(
         email=fake_email,
         first_name=second_fake_first_name,
         last_name=second_fake_last_name,
     )
 
+    # Checking error
     assert fail_insert == f'Error: Email "{fake_email}" is already registered'
+
+    # Selecting fake registry for delete
+    query = customer_repository.select(email=fake_email)
+    # Cleaning DB
+    customer_repository.delete(id=(query[0].id))
 
 
 def test_select():
+    # Fake payload
     fake_email = fake.email()
     fake_first_name = fake.first_name()
     fake_last_name = fake.last_name()
+    # Inserting fake
     customer_repository.insert(
         email=fake_email, first_name=fake_first_name, last_name=fake_last_name
     )
 
+    # Selecting fake registry
     query = customer_repository.select(
         email=fake_email,
         first_name=fake_first_name,
@@ -56,35 +76,47 @@ def test_select():
         is_active=True,
     )
 
+    # Checking equalities
     assert query[0].email == fake_email
     assert query[0].first_name == fake_first_name
     assert query[0].last_name == fake_last_name
     assert query[0].is_active == True
 
+    # Cleaning DB
+    customer_repository.delete(id=(query[0].id))
+
 
 def test_select_ParamIsNotIntegerError():
-
+    # Setting a not integer id
     fake_not_int_id = fake.word()
+    # Trying to select a customer by passing a not integer as id
     query = customer_repository.select(id=fake_not_int_id)
 
+    # Checking error
     assert query == f'Error: Param "{fake_not_int_id}" must be a integer'
 
 
 def test_select_ParamIsNotStringError():
-
+    # Setting a not string email
     fake_not_string_email = fake.random_digit()
+    # Trying to select a customer by passing a not string as email
     wrong_email_query = customer_repository.select(email=fake_not_string_email)
 
+    # Setting a not string first_name
     fake_not_string_first_name = fake.random_digit()
+    # Trying to select a customer by passing a not string as first_name
     wrong_first_name_query = customer_repository.select(
         email=fake_not_string_first_name
     )
 
+    # Setting a not string last_name
     fake_not_string_last_name = fake.random_digit()
+    # Trying to select a customer by passing a not string as last_name
     wrong_last_name_query = customer_repository.select(
         email=fake_not_string_last_name
     )
 
+    # Checking errors
     assert (
         wrong_email_query
         == f'Error: Param "{fake_not_string_email}" must be a string'
@@ -100,8 +132,46 @@ def test_select_ParamIsNotStringError():
 
 
 def test_select_ParamIsNotBoolError():
-
+    # Setting a not boolean is_active
     fake_not_bool_is_active = fake.word()
+    # Trying to select a customer by passing a not boolean as is_active
     query = customer_repository.select(is_active=fake_not_bool_is_active)
 
+    # Checking errors
     assert query == f'Error: Param {fake_not_bool_is_active} must be a boolean'
+
+
+def test_update():
+    # Fake payload
+    fake_email = fake.email()
+    fake_first_name = fake.first_name()
+    fake_last_name = fake.last_name()
+    # Inserting fake registry
+    customer_repository.insert(
+        email=fake_email, first_name=fake_first_name, last_name=fake_last_name
+    )
+
+    # Selecting fake registry
+    query = customer_repository.select(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        is_active=True,
+    )
+
+    # Second fake payload
+    new_fake_first_name = fake.first_name()
+    new_fake_last_name = fake.last_name()
+    # Update fake registry with second fake payload parameters
+    customer_repository.update(
+        id=query[0].id,
+        first_name=new_fake_first_name,
+        last_name=new_fake_last_name,
+    )
+
+    # Selecting updated fake registry
+    updated_query = customer_repository.select(id=query[0].id)
+
+    # Check equalities
+    assert updated_query[0].first_name == new_fake_first_name
+    assert updated_query[0].last_name == new_fake_last_name
