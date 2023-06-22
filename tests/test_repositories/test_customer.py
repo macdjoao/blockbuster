a4efd695dcd5f362c39afcdbc25f4895b4c66426
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-22 19:06:22 -0300

    tests: CustomerRepository test_select

--- tests/test_repositories/test_customer.py
+++ tests/test_repositories/test_customer.py
@@ -39,3 +39,24 @@ def test_insert_EmailAlreadyRegisteredError():
     )
 
     assert fail_insert == f'Error: Email "{fake_email}" is already registered'
+
+
+def test_select():
+    fake_email = fake.email()
+    fake_first_name = fake.first_name()
+    fake_last_name = fake.last_name()
+    customer_repository.insert(
+        email=fake_email, first_name=fake_first_name, last_name=fake_last_name
+    )
+
+    query = customer_repository.select(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        is_active=True,
+    )
+
+    assert query[0].email == fake_email
+    assert query[0].first_name == fake_first_name
+    assert query[0].last_name == fake_last_name
+    assert query[0].is_active == True
