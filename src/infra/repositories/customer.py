3bdc45856232be2640fd39efdd40d68991934f25
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-21 23:38:59 -0300

    fix: Refatorando Customer repository

--- src/infra/repositories/customer.py
+++ src/infra/repositories/customer.py
@@ -1,57 +1,78 @@
 from src.infra.configs.session import session
 from src.infra.entities.customer import Customer as CustomerEntity
-from src.infra.repositories.errors.general import (IdNotFoundError,
-                                                   IncompleteParamsError,
-                                                   ParamAreNotRecognizedError)
-from src.infra.repositories.utils.general import (
-    id_not_found, param_is_not_a_recognized_attribute, params_is_none)
+from src.infra.repositories.errors.general import (EmailAlreadyRegisteredError,
+                                                   IdNotFoundError,
+                                                   IncompleteParamError,
+                                                   ParamIsNotBoolError,
+                                                   ParamIsNotIntegerError,
+                                                   ParamIsNotStringError)
+from src.infra.repositories.utils.general import (email_already_registered,
+                                                  id_not_found, param_is_none,
+                                                  param_is_not_a_bool,
+                                                  param_is_not_a_int,
+                                                  param_is_not_a_string)
+
+customer_entity = CustomerEntity()
 
 
-class Customer:
-    def insert(self, id: str, email: str, name: str):
-        # Trechos comentados devem ser implementados em services
+class CustomerRepository:
+    def insert(self, email: str, first_name: str, last_name: str):
         try:
-            # data_insert = CustomerEntity(
-            #     id=str(uuid.uuid1()),
-            #     email=email.lower(),
-            #     name=name.capitalize(),
-            # )
+            if email_already_registered(
+                session=session, object=customer_entity, email=email
+            ):
+                raise EmailAlreadyRegisteredError(email=email)
             data_insert = CustomerEntity(
-                id=id,
-                email=email,
-                name=name,
+                email=email.lower(),
+                first_name=first_name.capitalize(),
+                last_name=last_name.capitalize(),
             )
             session.add(data_insert)
             session.commit()
 
             return data_insert
 
-        except Exception as err:
+        except EmailAlreadyRegisteredError as err:
             session.rollback()
-            return err
+            return err.message
         finally:
             session.close()
 
     def select(
         self,
-        id: str = None,
+        id: int = None,
         email: str = None,
-        name: str = None,
+        first_name: str = None,
+        last_name: str = None,
         is_active: bool = None,
     ):
         try:
             custom_filter = session.query(CustomerEntity)
             if id is not None:
+                if param_is_not_a_int(id):
+                    raise ParamIsNotIntegerError(arg=id)
                 custom_filter = custom_filter.filter(CustomerEntity.id == id)
             if email is not None:
+                if param_is_not_a_string(email):
+                    raise ParamIsNotStringError(arg=email)
                 custom_filter = custom_filter.filter(
                     CustomerEntity.email == email
                 )
-            if name is not None:
+            if first_name is not None:
+                if param_is_not_a_string(first_name):
+                    raise ParamIsNotStringError(arg=first_name)
                 custom_filter = custom_filter.filter(
-                    CustomerEntity.name == name
+                    CustomerEntity.first_name == first_name
+                )
+            if last_name is not None:
+                if param_is_not_a_string(last_name):
+                    raise ParamIsNotStringError(arg=last_name)
+                custom_filter = custom_filter.filter(
+                    CustomerEntity.last_name == last_name
                 )
             if is_active is not None:
+                if param_is_not_a_bool(is_active):
+                    raise ParamIsNotBoolError(arg=is_active)
                 custom_filter = custom_filter.filter(
                     CustomerEntity.is_active == is_active
                 )
@@ -60,27 +81,63 @@ class Customer:
 
             return data_select
 
-        except Exception as err:
-            return err
+        except ParamIsNotIntegerError as err:
+            session.rollback()
+            return err.message
+        except ParamIsNotStringError as err:
+            session.rollback()
+            return err.message
+        except ParamIsNotBoolError as err:
+            session.rollback()
+            return err.message
         finally:
             session.close()
 
-    def update(self, id: str = None, **kwargs):
+    def update(
+        self,
+        id: int = None,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
+        is_active: bool = None,
+    ):
 
-        customer_entity = CustomerEntity()
         try:
-            if params_is_none(id):
-                raise IncompleteParamsError
-            if id_not_found(session=session, object=CustomerEntity, arg=id):
+            if param_is_none(id):
+                raise IncompleteParamError(arg=id)
+            if param_is_not_a_int(id):
+                raise ParamIsNotIntegerError(arg=id)
+            if id_not_found(session=session, object=customer_entity, arg=id):
                 raise IdNotFoundError(id=id)
-            for kwarg in kwargs:
-                if param_is_not_a_recognized_attribute(
-                    object=customer_entity, arg=kwarg
+            if email is not None:
+                if param_is_not_a_string(email):
+                    raise ParamIsNotStringError(arg=email)
+                if email_already_registered(
+                    session=session, object=customer_entity, email=email
                 ):
-                    raise ParamAreNotRecognizedError(error_param=kwarg)
+                    raise EmailAlreadyRegisteredError(email=email)
                 session.query(CustomerEntity).filter(
                     CustomerEntity.id == id
-                ).update({f'{kwarg}': kwargs[f'{kwarg}']})
+                ).update({'email': email})
+            if first_name is not None:
+                if param_is_not_a_string(first_name):
+                    raise ParamIsNotStringError(arg=first_name)
+                session.query(CustomerEntity).filter(
+                    CustomerEntity.id == id
+                ).update({'first_name': first_name})
+            if last_name is not None:
+                if param_is_not_a_string(last_name):
+                    raise ParamIsNotStringError(arg=last_name)
+                session.query(CustomerEntity).filter(
+                    CustomerEntity.id == id
+                ).update({'last_name': last_name})
+            if is_active is not None:
+                if param_is_not_a_bool(is_active):
+                    raise ParamIsNotBoolError(arg=is_active)
+                session.query(CustomerEntity).filter(
+                    CustomerEntity.id == id
+                ).update({'is_active': is_active})
+
             data_update = (
                 session.query(CustomerEntity)
                 .filter(CustomerEntity.id == id)
@@ -88,20 +145,35 @@ class Customer:
             )
             return data_update
 
-        except IncompleteParamsError as err:
+        except IncompleteParamError as err:
+            session.rollback()
+            return err.message
+        except ParamIsNotIntegerError as err:
             session.rollback()
             return err.message
         except IdNotFoundError as err:
             session.rollback()
             return err.message
-        except ParamAreNotRecognizedError as err:
+        except ParamIsNotStringError as err:
+            session.rollback()
+            return err.message
+        except EmailAlreadyRegisteredError as err:
+            session.rollback()
+            return err.message
+        except ParamIsNotBoolError as err:
             session.rollback()
             return err.message
         finally:
             session.close()
 
-    def delete(self, id: str):
+    def delete(self, id: int):
         try:
+            if param_is_none(id):
+                raise IncompleteParamError(arg=id)
+            if param_is_not_a_int(id):
+                raise ParamIsNotIntegerError(arg=id)
+            if id_not_found(session=session, object=customer_entity, arg=id):
+                raise IdNotFoundError(id=id)
 
             data_delete = (
                 session.query(CustomerEntity)
@@ -116,8 +188,14 @@ class Customer:
 
             return data_delete
 
-        except Exception as err:
+        except IncompleteParamError as err:
             session.rollback()
-            return err
+            return err.message
+        except ParamIsNotIntegerError as err:
+            session.rollback()
+            return err.message
+        except IdNotFoundError as err:
+            session.rollback()
+            return err.message
         finally:
             session.close()
