3f1d01aa33b89a0599c5be56125d3baf4a9a6325
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-21 22:53:03 -0300

    feat: Adicionando novos campos de user e costumer

--- src/infra/entities/customer.py
+++ src/infra/entities/customer.py
@@ -8,8 +8,9 @@ class Customer(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False, unique=True)
-    name = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
 
     def __repr__(self):
-        return f'Customer (id = {self.id}, email = {self.email}, name = {self.name}, is_active = {self.is_active})'
+        return f'Customer (id = {self.id}, email = {self.email}, first_name = {self.first_name}, last_name = {self.last_name}, is_active = {self.is_active})'
--- src/infra/entities/user.py
+++ src/infra/entities/user.py
@@ -8,9 +8,10 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False, unique=True)
-    name = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     password = Column(String, nullable=False)
 
     def __repr__(self):
-        return f'User (id = {self.id}, email = {self.email}, name = {self.name}, is_active = {self.is_active})'
+        return f'User (id = {self.id}, email = {self.email}, first_name = {self.first_name}, last_name = {self.last_name}, is_active = {self.is_active})'
