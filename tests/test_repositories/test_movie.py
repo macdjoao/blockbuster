c5f93fcce07d21ccd8ba7cb1c8ca51af0a9bb8bd
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-25 19:56:57 -0300

    tests: test_insert_ParamIsNotStringError

--- tests/test_repositories/test_movie.py
+++ tests/test_repositories/test_movie.py
@@ -22,3 +22,17 @@ def test_insert():
 
     # Cleaning DB
     movie_repository.delete(id=(query[0].id))
+
+
+def test_insert_ParamIsNotStringError():
+    # Setting a not string value
+    fake_not_string = fake.random_digit()
+
+    # Trying to insert by passing a not string value as name
+    wrong_name_insert = movie_repository.insert(name=fake_not_string)
+
+    # Checking errors
+    assert (
+        wrong_name_insert
+        == f'Error: Param "{fake_not_string}" must be a string'
+    )
