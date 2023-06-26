0b3b531daacf3e6d08e09356da6808a3881cf010
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-26 14:01:37 -0300

    tests: test_insert_ParamIsNotStringError de UserRepository

--- tests/test_repositories/test_user.py
+++ tests/test_repositories/test_user.py
@@ -67,3 +67,64 @@ def test_insert_EmailAlreadyRegisteredError():
     query = user_repository.select(email=fake_email)
     # Cleaning DB
     user_repository.delete(id=(query[0].id))
+
+
+def test_insert_ParamIsNotStringError():
+    # Fake payload
+    fake_email = fake.email()
+    fake_first_name = fake.first_name()
+    fake_last_name = fake.last_name()
+    fake_password = fake.word()
+
+    # Setting a not string value
+    fake_not_string = fake.random_digit()
+
+    # Trying to insert by passing a not string value as email
+    wrong_email_insert = user_repository.insert(
+        email=fake_not_string,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        password=fake_password,
+    )
+
+    # Trying to insert by passing a not string value as first_name
+    wrong_first_name_insert = user_repository.insert(
+        email=fake_email,
+        first_name=fake_not_string,
+        last_name=fake_last_name,
+        password=fake_password,
+    )
+
+    # Trying to insert by passing a not string value as last_name
+    wrong_last_name_insert = user_repository.insert(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_not_string,
+        password=fake_password,
+    )
+
+    # Trying to insert by passing a not string value as password
+    wrong_password_insert = user_repository.insert(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        password=fake_not_string,
+    )
+
+    # Checking errors
+    assert (
+        wrong_email_insert
+        == f'Error: Param "{fake_not_string}" must be a string'
+    )
+    assert (
+        wrong_first_name_insert
+        == f'Error: Param "{fake_not_string}" must be a string'
+    )
+    assert (
+        wrong_last_name_insert
+        == f'Error: Param "{fake_not_string}" must be a string'
+    )
+    assert (
+        wrong_password_insert
+        == f'Error: Param "{fake_not_string}" must be a string'
+    )
