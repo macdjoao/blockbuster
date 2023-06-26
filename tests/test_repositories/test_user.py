3bffbdc32cc8223e83c13c1c5a8c1d35a50a3240
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-26 14:13:34 -0300

    tests: test_insert de UserRepository

--- tests/test_repositories/test_user.py
+++ tests/test_repositories/test_user.py
@@ -128,3 +128,35 @@ def test_insert_ParamIsNotStringError():
         wrong_password_insert
         == f'Error: Param "{fake_not_string}" must be a string'
     )
+
+
+def test_select():
+    # Fake payload
+    fake_email = fake.email()
+    fake_first_name = fake.first_name()
+    fake_last_name = fake.last_name()
+    fake_password = fake.word()
+    # Inserting fake
+    user_repository.insert(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        password=fake_password,
+    )
+
+    # Selecting fake registry
+    query = user_repository.select(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        is_active=True,
+    )
+
+    # Checking equalities
+    assert query[0].email == fake_email
+    assert query[0].first_name == fake_first_name
+    assert query[0].last_name == fake_last_name
+    assert query[0].is_active == True
+
+    # Cleaning DB
+    user_repository.delete(id=(query[0].id))
