ae8272f1dc753327c2a802321b6850cdb4379bb5
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-26 14:49:17 -0300

    tests: test_update_ParamIsNotStringError de UserRepository

--- tests/test_repositories/test_user.py
+++ tests/test_repositories/test_user.py
@@ -271,3 +271,60 @@ def test_update_ParamIsNotIntegerError():
 
     # Checking error
     assert update == f'Error: Param "{fake_wrong_id}" must be a integer'
+
+
+def test_update_ParamIsNotStringError():
+    # Fake payload
+    fake_email = fake.email()
+    fake_first_name = fake.first_name()
+    fake_last_name = fake.last_name()
+    fake_password = fake.word()
+    # Inserting fake registry
+    user_repository.insert(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        password=fake_password,
+    )
+
+    # Selecting fake registry
+    query = user_repository.select(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        is_active=True,
+    )
+
+    # Not string value
+    not_string_value = fake.random_digit()
+
+    # Trying update fake registry with not string value
+    wrong_email = user_repository.update(
+        id=query[0].id, email=not_string_value
+    )
+    wrong_first_name = user_repository.update(
+        id=query[0].id, first_name=not_string_value
+    )
+    wrong_last_name = user_repository.update(
+        id=query[0].id, last_name=not_string_value
+    )
+    wrong_password = user_repository.update(
+        id=query[0].id, password=not_string_value
+    )
+
+    # Checking errors
+    assert wrong_email == f'Error: Param "{not_string_value}" must be a string'
+    assert (
+        wrong_first_name
+        == f'Error: Param "{not_string_value}" must be a string'
+    )
+    assert (
+        wrong_last_name
+        == f'Error: Param "{not_string_value}" must be a string'
+    )
+    assert (
+        wrong_password == f'Error: Param "{not_string_value}" must be a string'
+    )
+
+    # Cleaning DB
+    user_repository.delete(id=(query[0].id))
