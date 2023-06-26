0f6fe013460e9cc6b59943e035905871bb0e4cd1
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-26 13:57:39 -0300

    tests: test_insert_EmailAlreadyRegisteredError de UserRepository

--- tests/test_repositories/test_user.py
+++ tests/test_repositories/test_user.py
@@ -31,3 +31,39 @@ def test_insert():
 
     # Cleaning DB
     user_repository.delete(id=(query[0].id))
+
+
+def test_insert_EmailAlreadyRegisteredError():
+    # Fake payload
+    fake_email = fake.email()
+    fake_first_name = fake.first_name()
+    fake_last_name = fake.last_name()
+    fake_password = fake.word()
+
+    # Inserting fake registry
+    user_repository.insert(
+        email=fake_email,
+        first_name=fake_first_name,
+        last_name=fake_last_name,
+        password=fake_password,
+    )
+
+    # Second fake Payload
+    second_fake_first_name = fake.first_name()
+    second_fake_last_name = fake.last_name()
+
+    # Trying insert second fake registry with same email of first fake registry
+    fail_insert = user_repository.insert(
+        email=fake_email,
+        first_name=second_fake_first_name,
+        last_name=second_fake_last_name,
+        password=fake_password,
+    )
+
+    # Checking error
+    assert fail_insert == f'Error: Email "{fake_email}" is already registered'
+
+    # Selecting fake registry for delete
+    query = user_repository.select(email=fake_email)
+    # Cleaning DB
+    user_repository.delete(id=(query[0].id))
