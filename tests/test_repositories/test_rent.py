781903655c586dc93c25a2cea010cf34c80048b5
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-27 16:36:55 -0300

    tests: test_update_ParamIsNotIntegerError de RentRepository

--- tests/test_repositories/test_rent.py
+++ tests/test_repositories/test_rent.py
@@ -240,3 +240,85 @@ def test_update():
     movie_repository.delete(id=second_movie_query[0].id)
     user_repository.delete(id=user_query[0].id)
     customer_repository.delete(id=customer_query[0].id)
+
+
+def test_update_ParamIsNotIntegerError():
+    # Fake payload to user
+    user_fake_email = fake.email()
+    user_fake_first_name = fake.first_name()
+    user_fake_last_name = fake.last_name()
+    user_fake_password = fake.word()
+    # Inserting fake user registry
+    user_repository.insert(
+        email=user_fake_email,
+        first_name=user_fake_first_name,
+        last_name=user_fake_last_name,
+        password=user_fake_password,
+    )
+    # Selecting fake user registry
+    user_query = user_repository.select(email=user_fake_email)
+
+    # Fake payload to customer
+    customer_fake_email = fake.email()
+    customer_fake_first_name = fake.first_name()
+    customer_fake_last_name = fake.last_name()
+    # Inserting fake registry
+    customer_repository.insert(
+        email=customer_fake_email,
+        first_name=customer_fake_first_name,
+        last_name=customer_fake_last_name,
+    )
+    # Selecting fake customer registry
+    customer_query = customer_repository.select(email=customer_fake_email)
+
+    # Fake payload to movie
+    movie_fake_name = fake.word()
+    # Inserting fake movie registry
+    movie_repository.insert(name=movie_fake_name)
+    # Selecting fake movie registry
+    movie_query = movie_repository.select(name=movie_fake_name, available=True)
+
+    # Fake payload to rent
+    fake_devolution_date = datetime.datetime(2025, 1, 15)
+    # Inserting fake rent registry
+    rent_repository.insert(
+        user_id=user_query[0].id,
+        customer_id=customer_query[0].id,
+        movie_id=movie_query[0].id,
+        devolution_date=fake_devolution_date,
+    )
+    # Selecting fake rent registry
+    rent_query = rent_repository.select(
+        user_id=user_query[0].id,
+        customer_id=customer_query[0].id,
+        movie_id=movie_query[0].id,
+        devolution_date=fake_devolution_date,
+    )
+
+    fake_not_int = fake.word()
+
+    wrong_id = rent_repository.update(id=fake_not_int)
+    wrong_user_id = rent_repository.update(
+        id=rent_query[0].id, user_id=fake_not_int
+    )
+    wrong_customer_id = rent_repository.update(
+        id=rent_query[0].id, customer_id=fake_not_int
+    )
+    wrong_movie_id = rent_repository.update(
+        id=rent_query[0].id, movie_id=fake_not_int
+    )
+    # Updated query
+
+    # Checking errors
+    assert wrong_id == f'Error: Param "{fake_not_int}" must be a integer'
+    assert wrong_user_id == f'Error: Param "{fake_not_int}" must be a integer'
+    assert (
+        wrong_customer_id == f'Error: Param "{fake_not_int}" must be a integer'
+    )
+    assert wrong_movie_id == f'Error: Param "{fake_not_int}" must be a integer'
+
+    # Cleaning DB
+    rent_repository.delete(id=rent_query[0].id)
+    movie_repository.delete(id=movie_query[0].id)
+    user_repository.delete(id=user_query[0].id)
+    customer_repository.delete(id=customer_query[0].id)
