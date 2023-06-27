5a2f49b8396bb963d3aae52a4e9c17ffc7679773
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-27 16:43:34 -0300

    tests: test_update_ParamIsNotDateError de RentRepository

--- tests/test_repositories/test_rent.py
+++ tests/test_repositories/test_rent.py
@@ -297,7 +297,6 @@ def test_update_ParamIsNotIntegerError():
 
     # Setting a not integer value
     fake_not_int = fake.word()
-
     # Trying update rent with not integer values
     wrong_id = rent_repository.update(id=fake_not_int)
     wrong_user_id = rent_repository.update(
@@ -316,6 +315,83 @@ def test_update_ParamIsNotIntegerError():
     assert wrong_movie_id == f'Error: Param "{fake_not_int}" must be a integer'
     assert (
         wrong_customer_id == f'Error: Param "{fake_not_int}" must be a integer'
+    )
+
+    # Cleaning DB
+    rent_repository.delete(id=rent_query[0].id)
+    movie_repository.delete(id=movie_query[0].id)
+    user_repository.delete(id=user_query[0].id)
+    customer_repository.delete(id=customer_query[0].id)
+
+
+def test_update_ParamIsNotDateError():
+    # Fake payload to user
+    user_fake_email = fake.email()
+    user_fake_first_name = fake.first_name()
+    user_fake_last_name = fake.last_name()
+    user_fake_password = fake.word()
+    # Inserting fake user registry
+    user_repository.insert(
+        email=user_fake_email,
+        first_name=user_fake_first_name,
+        last_name=user_fake_last_name,
+        password=user_fake_password,
+    )
+    # Selecting fake user registry
+    user_query = user_repository.select(email=user_fake_email)
+
+    # Fake payload to customer
+    customer_fake_email = fake.email()
+    customer_fake_first_name = fake.first_name()
+    customer_fake_last_name = fake.last_name()
+    # Inserting fake registry
+    customer_repository.insert(
+        email=customer_fake_email,
+        first_name=customer_fake_first_name,
+        last_name=customer_fake_last_name,
+    )
+    # Selecting fake customer registry
+    customer_query = customer_repository.select(email=customer_fake_email)
+
+    # Fake payload to movie
+    movie_fake_name = fake.word()
+    # Inserting fake movie registry
+    movie_repository.insert(name=movie_fake_name)
+    # Selecting fake movie registry
+    movie_query = movie_repository.select(name=movie_fake_name, available=True)
+
+    # Fake payload to rent
+    fake_devolution_date = datetime.datetime(2025, 1, 15)
+    # Inserting fake rent registry
+    rent_repository.insert(
+        user_id=user_query[0].id,
+        customer_id=customer_query[0].id,
+        movie_id=movie_query[0].id,
+        devolution_date=fake_devolution_date,
+    )
+    # Selecting fake rent registry
+    rent_query = rent_repository.select(
+        user_id=user_query[0].id,
+        customer_id=customer_query[0].id,
+        movie_id=movie_query[0].id,
+        devolution_date=fake_devolution_date,
+    )
+
+    # Setting a not date value
+    fake_not_date = fake.random_digit()
+    # Trying update rent with not date values
+    wrong_rent_date = rent_repository.update(
+        id=rent_query[0].id, rent_date=fake_not_date
+    )
+    wrong_devolution_date = rent_repository.update(
+        id=rent_query[0].id, devolution_date=fake_not_date
+    )
+
+    # Checking errors
+    assert wrong_rent_date == f'Error: Param "{fake_not_date}" must be a date'
+    assert (
+        wrong_devolution_date
+        == f'Error: Param "{fake_not_date}" must be a date'
     )
 
     # Cleaning DB
