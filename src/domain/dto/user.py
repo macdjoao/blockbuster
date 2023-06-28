9f8646379a1f9cb1b9fee64b8acef4d4badc1182
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-28 17:53:01 -0300

    feat: Primeiros schemas e desenhando primeiro service

--- src/domain/dto/user.py
+++ src/domain/dto/user.py
@@ -6,3 +6,15 @@ class CreateUser(BaseModel):
     first_name: str
     last_name: str
     password: str
+
+
+class CreatedUser(BaseModel):
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    is_active: bool
+
+
+class ReadUsers(BaseModel):
+    id: int
--- src/domain/services/user.py
+++ src/domain/services/user.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from src.auth.hashes import Hashes
-from src.domain.dto.user import CreateUser
+from src.domain.dto.user import CreatedUser, CreateUser
 from src.infra.repositories.user import UserRepository
 
 hashes = Hashes()
@@ -8,21 +10,39 @@ user_repository = UserRepository()
 
 class UserService:
     def create(self, schema: CreateUser):
-        formated_email = (schema.email).lower()
-        formated_first_name = (schema.first_name).capitalize()
-        formated_last_name = (schema.last_name).capitalize()
-        hashed_password = hashes.generate_password_hash(
-            password=(schema.password)
-        )
+        try:
+            formated_email = (schema.email).lower()
+            formated_first_name = (schema.first_name).capitalize()
+            formated_last_name = (schema.last_name).capitalize()
+            hashed_password = hashes.generate_password_hash(
+                password=(schema.password)
+            )
 
-        user_repository.insert(
-            email=formated_email,
-            first_name=formated_first_name,
-            last_name=formated_last_name,
-            password=hashed_password,
-        )
+            user_repository.insert(
+                email=formated_email,
+                first_name=formated_first_name,
+                last_name=formated_last_name,
+                password=hashed_password,
+            )
+
+            data_select = user_repository.select(
+                email=formated_email,
+                first_name=formated_first_name,
+                last_name=formated_last_name,
+            )
+
+            return CreatedUser(
+                id=data_select[0].id,
+                email=data_select[0].email,
+                first_name=data_select[0].first_name,
+                last_name=data_select[0].last_name,
+                is_active=data_select[0].is_active,
+            )
+
+        except Exception as err:
+            return err
 
 
 test = UserService()
-x = CreateUser(email='oi', first_name='oi', last_name='oi', password='oi')
-test.create(schema=x)
+x = CreateUser(email='opawq', first_name='oi', last_name='oi', password='oi')
+print(test.create(schema=x))
